de238c7cb4e9bf87feef2948322409e31d870caa
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-11-01 22:39:26 +0200

    BMI example.

--- app/site/application.js
+++ app/site/application.js
@@ -121,6 +121,40 @@ const Login = ({usernameAtom, passwordAtom, loginStatusStream, login, logout}) =
 
 ////////////////////////////////////////////////////////////////////////////////
 
+const BMIModel = () => {
+  const weightAtom = Atom(70)
+  const heightAtom = Atom(170)
+  return {weightAtom,
+          heightAtom,
+          bmiStream: Bacon.combineWith(weightAtom, heightAtom, (w, h) =>
+                                       Math.round(w/(h * h * 0.0001)))}
+}
+
+const BMI = ({weightAtom, heightAtom, bmiStream}) => {
+  const Slider = (title, units, min, max, atom) =>
+    atom.map(value =>
+      <div>
+        {title}: {value}{units}
+        <div>
+          <input type="range" min={min} max={max} value={value}
+             onChange={e => atom.reset(e.target.value)}/>
+        </div>
+      </div>)
+
+  return Bacon.combineWith(
+    Slider("Weight", "kg", 40, 140, weightAtom),
+    Slider("Height", "cm", 140, 210, heightAtom),
+    bmiStream,
+    (weightSlider, heightSlider, bmi) =>
+      <div>
+        {weightSlider}
+        {heightSlider}
+        BMI: {bmi}
+      </div>)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 export default () => {
 
   const pages =
@@ -130,6 +164,9 @@ export default () => {
      {value: "ThreeCounters",
       path: "/page/three-counters",
       DOMs: ThreeCounters(Atom(0))},
+     {value: "BMI",
+      path: "/page/bmi",
+      DOMs: BMI(BMIModel())},
      {value: "Login",
       path: "/page/login",
       DOMs: Login(LoginModel())}]
