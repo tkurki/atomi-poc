f059993ca17d9618a8b037d0bf0fa7cbc77bd172
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-11-03 07:15:27 +0200

    Refactored ThreeWayBMI into a separate module.

--- app/site/application.js
+++ app/site/application.js
@@ -2,6 +2,7 @@ import React from "react"
 import Bacon from "baconjs"
 import {Model} from "bacon.model"
 import {findIndex, flatten, map} from "lodash"
+import * as ThreeWayBMI from "./components/three-way-bmi"
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -146,53 +147,6 @@ const BMI = ({weightModel, heightModel, bmiStream}) => {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-const ThreeWayBMIModel = () => {
-  const complete = m =>
-    !m.bmi ? {...m, bmi: Math.round(m.weight / (m.height * m.height * 0.0001))}
-    : !m.height ? {...m, height: Math.round(Math.sqrt(m.weight / m.bmi) * 100)}
-    : {...m, weight: Math.round(m.bmi * m.height * m.height * 0.0001)}
-
-  const modelModel = Model({weight: 70, height: 170})
-
-  return {modelModel, completedStream: modelModel.map(complete)}
-}
-
-const ThreeWayBMI = ({modelModel, completedStream}) => {
-  const lockedModel = Model('bmi')
-
-  const Slider = (title, units, prop, min, max) =>
-    Bacon.combineWith(completedStream, lockedModel, (c, locked) =>
-      <div>
-        <input type="checkbox"
-               onChange={_ => lockedModel.set(prop)}
-               disabled={locked === prop}
-               checked={locked === prop}/>
-        {title}: {c[prop]}{units}
-        <div>
-          <input type="range" min={min} max={max} value={c[prop]}
-             disabled={locked === prop}
-             onChange={e => {
-               const m = {weight: c.weight, height: c.height, bmi: c.bmi}
-               delete m[locked]
-               m[prop] = e.target.value
-               modelModel.set(m)}}/>
-        </div>
-      </div>)
-
-  return Bacon.combineWith(
-    Slider("Weight", "kg", 'weight', 40, 140),
-    Slider("Height", "cm", 'height', 140, 210),
-    Slider("BMI: ", "kg/m^2", 'bmi', 10, 40),
-    (weightSlider, heightSlider, bmiSlider) =>
-      <div>
-        {weightSlider}
-        {heightSlider}
-        {bmiSlider}
-      </div>)
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 const ComponentList = componentsModel => {
   const componentCreates =
     [{value: "ClassyCounter",
@@ -273,7 +227,7 @@ export default () => {
       DOMs: BMI(BMIModel())},
      {value: "3-way BMI",
       path: "/page/3-way-bmi",
-      DOMs: ThreeWayBMI(ThreeWayBMIModel())},
+      DOMs: ThreeWayBMI.WebControl(ThreeWayBMI.Model())},
      {value: "Login",
       path: "/page/login",
       DOMs: Login(LoginModel())},
---
+++ app/site/components/three-way-bmi.js
@@ -0,0 +1,48 @@
+import React from "react"
+import Bacon from "baconjs"
+import {Model as Atom} from "bacon.model"
+
+export const Model = () => {
+  const complete = m =>
+    !m.bmi ? {...m, bmi: Math.round(m.weight / (m.height * m.height * 0.0001))}
+    : !m.height ? {...m, height: Math.round(Math.sqrt(m.weight / m.bmi) * 100)}
+    : {...m, weight: Math.round(m.bmi * m.height * m.height * 0.0001)}
+
+  const partialAtom = Atom({weight: 70, height: 170})
+
+  return {partialAtom, completedStream: partialAtom.map(complete)}
+}
+
+export const WebControl = ({partialAtom, completedStream}) => {
+  const lockedAtom = Atom('bmi')
+
+  const Slider = (title, units, prop, min, max) =>
+    Bacon.combineWith(completedStream, lockedAtom, (c, locked) =>
+      <div>
+        <input type="checkbox"
+               onChange={_ => lockedAtom.set(prop)}
+               disabled={locked === prop}
+               checked={locked === prop}/>
+        {title}: {c[prop]}{units}
+        <div>
+          <input type="range" min={min} max={max} value={c[prop]}
+             disabled={locked === prop}
+             onChange={e => {
+               const m = {weight: c.weight, height: c.height, bmi: c.bmi}
+               delete m[locked]
+               m[prop] = e.target.value
+               partialAtom.set(m)}}/>
+        </div>
+      </div>)
+
+  return Bacon.combineWith(
+    Slider("Weight", "kg", 'weight', 40, 140),
+    Slider("Height", "cm", 'height', 140, 210),
+    Slider("BMI: ", "kg/m^2", 'bmi', 10, 40),
+    (weightSlider, heightSlider, bmiSlider) =>
+      <div>
+        {weightSlider}
+        {heightSlider}
+        {bmiSlider}
+      </div>)
+}
